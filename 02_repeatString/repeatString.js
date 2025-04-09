dd920d24944304d8a805fcb9192d1a421a8ed48a
Oscar <odgarrett@ucdavis.edu>
2025-04-09 07:06:39 -0700

    Completes 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  combined_string = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    combined_string += String(string);
+  }
+  return combined_string;
 };
 
 // Do not edit below this line
