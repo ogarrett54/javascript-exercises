2ad8975028e61652b410d4d8e8514246c0d4c8ba
Oscar <odgarrett@ucdavis.edu>
2025-04-09 07:13:06 -0700

    Completes 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedString = "";
+  for (let i = 1; i <= string.length; i++) {
+    reversedString += string.at(-i);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
