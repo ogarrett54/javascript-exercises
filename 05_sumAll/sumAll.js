0c232e9df4b3f6243d166dac2fc0fcc40b8c1ef4
Oscar <odgarrett@ucdavis.edu>
2025-04-09 20:49:31 -0700

    Completes 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (intOne, intTwo) {
+  let sum = 0;
 
+  if (
+    !Number.isInteger(intOne) ||
+    intOne < 0 ||
+    !Number.isInteger(intTwo) ||
+    intTwo < 0
+  ) {
+    return "ERROR";
+  }
+
+  const start = Math.min(intOne, intTwo);
+  const end = Math.max(intOne, intTwo);
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
