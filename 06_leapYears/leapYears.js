08040df67ddf719dd40e3d7c31442e3ea1489e0b
Oscar <odgarrett@ucdavis.edu>
2025-04-09 20:56:11 -0700

    Completes 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (Number(year) % 4 === 0) {
+    if (Number(year) % 100 === 0) {
+      if (Number(year) % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
