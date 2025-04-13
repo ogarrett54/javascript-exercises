f0d69f0b65370d2d81a4cf63daf2fa17f158a2c2
Oscar <odgarrett@ucdavis.edu>
2025-04-13 08:37:26 -0700

    Completes 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  let fibSeq = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    const Fi = fibSeq[i - 1] + fibSeq[i - 2];
+    fibSeq.push(Fi);
+  }
+  return fibSeq[n];
 };
 
 // Do not edit below this line
