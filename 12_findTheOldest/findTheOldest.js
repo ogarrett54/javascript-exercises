ed62435eff53d75b7275d2f55d472849a4edc994
Oscar <odgarrett@ucdavis.edu>
2025-04-13 08:56:10 -0700

    Completes 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let sorted = arr.slice(0, arr.length);
+  sorted.sort((a, b) => {
+    if (!a.yearOfDeath) {
+      a.yearOfDeath = new Date().getFullYear();
+    } else if (!b.yearOfDeath) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    return -(a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth));
+  });
+  return sorted[0];
 };
 
 // Do not edit below this line
