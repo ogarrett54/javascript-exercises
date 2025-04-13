807fe07683da7d0332761b4224ece71cf62f5516
Oscar <odgarrett@ucdavis.edu>
2025-04-13 08:19:03 -0700

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,45 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const split = string.toLowerCase().split("");
+  const alphanumeric = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  const lettersOnly = split.filter((char) => alphanumeric.includes(char));
+  return lettersOnly.join() == lettersOnly.reverse().join();
 };
 
 // Do not edit below this line
